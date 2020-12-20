bfff0394935f704e834d353d0c6b20a8151ab3d9
111sunil <SuniLch@111>
2020-12-20 09:41:20 +0530

    Initial Commit

---
+++ src/main/java/guru/springframework/sfgdi/services/ConstructorInjectedGreetingService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorInjectedGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+
+        return "Hello World - Constructor";
+    }
+}
